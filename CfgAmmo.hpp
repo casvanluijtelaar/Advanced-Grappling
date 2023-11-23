445c5e8c8ee9fdd66e6ed2084674f055512ab46a
Cas van Luijtelaar <67225314+casvanluijtelaar@users.noreply.github.com>
2023-11-23 19:21:28 +0100

    cfgammo

--- CfgAmmo.hpp
+++ CfgAmmo.hpp
@@ -1,73 +1,102 @@
-class CfgAmmo {
+class CfgAmmo
+{
 
 	class GrenadeBase;
-	class Default;	// External class reference
-	class Grenade : Default {};
-	class GrenadeHand : Grenade {};
 
-	// throwable grappling hook
-	class G_GRAPPEL  : GrenadeHand  {
-		brightness = 0;
-		explosionSoundEffect = "";
-		simulation = "shotShell";
-		model = "\A3\weapons_f\ammo\flare_white";
-		hit = 0;
-		indirectHit = 0;
-		indirectHitRange = 0;
-		warheadName = "GRAPPEL";
-		visibleFire = 0;
-		audibleFire = 0;
-		visibleFireTime = 0;
-		dangerRadiusHit = -1;
-		deflectionSlowDown = 0;
-		suppressionRadiusHit = 0;
-		explosive = 0;
-		explosionTime = 0;
-		cost = 10;
-		deflecting = 0;
-		airFriction = -0.001;
-		fuseDistance = 1;
-		whistleDist = 0;    // no BIS explosion effects
-        whistleOnFire = 0;  // no BIS firing effects
-		typicalSpeed = 180;
-		caliber = 2;
+    // throwable grappling hook
+    class G_Grappling_Hook : GrenadeBase
+    {
+        aiAmmoUsageFlags = "0"; // no AI usage
+        cost = 40;
+        dangerRadiusHit = 0;
+        deflecting = 0;
+        explosionTime = -1;
+        explosive = 0;
+        grenadeBurningSound[] = {};
+        grenadeFireSound[] = {};
+        hit = 0;
+        indirectHit = 0;
+        model = "\A3\weapons_f\ammo\flare_white";
+        simulation = "shotShell";
+        soundHit[] = {"",0,0};
+        soundFly[] = {"",0,0};
+        soundEngine[] = {"",0,0};
+        suppressionRadiusHit = 0;
+        timeToLive = 10;
+        typicalSpeed = 22;
+        warheadName = "GRAPPLING HOOK";
+        whistleDist = 0;
+
+        class CamShakeFire
+        {
+            power = 0;
+            duration = 0;
+            frequency = 0;
+            distance = 0;
+        };
+        class CamShakePlayerFire
+        {
+            power = 0;
+            duration = 0;
+            frequency = 0;
+        };
+        class CamShakeHit
+        {
+            power = 0;
+            duration = 0;
+            frequency = 0;
+        };
+        
+    };
+
+    // grenade launcher grappling hook
+    class G_40mm_Grappling_Hook : GrenadeBase
+    {
+        aiAmmoUsageFlags = "0"; // no AI usage
+        cost = 40;
+        dangerRadiusHit = 0;
+        deflecting = 0;
+        explosionTime = -1;
+        explosive = 0;
+        grenadeBurningSound[] = {};
+        grenadeFireSound[] = {};
+        hit = 0;
+        indirectHit = 0;
+        maxSpeed = 100;
+        model = "\A3\weapons_f\ammo\UGL_slug";
+        simulation = "shotShell";
+        soundHit[] = {"",0,0};
+        soundFly[] = {"",0,0};
+        soundEngine[] = {"",0,0};
+        suppressionRadiusHit = 0;
+        timeToLive = 10;
+        typicalSpeed = 22;
+        warheadName = "GRAPPLING HOOK";
+        whistleDist = 0;
 		soundHit1[] = {};
 		soundHit2[] = {};
 		soundHit3[] = {};
 		soundHit4[] = {};
 		multiSoundHit[] = {"soundHit1",0.25,"soundHit2",0.25,"soundHit3",0.25,"soundHit4",0.25};
-	};
-
-	// grenade launcher grappling hook
-	class G_40mm_GRAPPEL : GrenadeBase {
-		brightness = 0;
-		explosionSoundEffect = "";
-		simulation = "shotShell";
-		model = "\A3\weapons_f\ammo\UGL_slug";
-		hit = 0;
-		indirectHit = 0;
-		indirectHitRange = 0;
-		warheadName = "GRAPPEL";
-		visibleFire = 0;
-		audibleFire = 0;
-		visibleFireTime = 0;
-		dangerRadiusHit = -1;
-		deflectionSlowDown = 0;
-		suppressionRadiusHit = 0;
-		explosive = 0;
-		explosionTime = 0;
-		cost = 10;
-		deflecting = 0;
-		airFriction = -0.01;
-		fuseDistance = 1;
-		whistleDist = 0;  
-        whistleOnFire = 0;
-		typicalSpeed = 180;
-		caliber = 2;
-		soundHit1[] = {};
-		soundHit2[] = {};
-		soundHit3[] = {};
-		soundHit4[] = {};
-		multiSoundHit[] = {"soundHit1",0.25,"soundHit2",0.25,"soundHit3",0.25,"soundHit4",0.25};
-	};
+    
+        class CamShakeFire
+        {
+            power = 0;
+            duration = 0;
+            frequency = 0;
+            distance = 0;
+        };
+        class CamShakePlayerFire
+        {
+            power = 0;
+            duration = 0;
+            frequency = 0;
+        };
+        class CamShakeHit
+        {
+            power = 0;
+            duration = 0;
+            frequency = 0;
+        };
+    };
 };
